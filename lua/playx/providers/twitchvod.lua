aac1b7abe4b5037c504df85f395ed79c68183c56
Júlio C. Oliveira <talk@juliocesar.me>
2023-02-09 09:50:46 -0300

    Updated Copyright years.

--- lua/playx/providers/twitchvod.lua
+++ lua/playx/providers/twitchvod.lua
@@ -1,6 +1,6 @@
  -- PlayX
 -- Copyright (c) 2009, 2010 sk89q <http://www.sk89q.com>
--- Copyright (c) 2011 - 2021 DathusBR <https://www.juliocesar.me>
+-- Copyright (c) 2011 - 2023 DathusBR <https://www.juliocesar.me>
 -- 
 -- This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 -- To view a copy of this license, visit Common Creative's Website. <https://creativecommons.org/licenses/by-nc-sa/4.0/>
